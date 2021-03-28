f01f531ed08e4e0024ea0f96d559ea2fa48726ab
Mudassir Khan <38735197+Muddi00seven@users.noreply.github.com>
2021-03-28 18:02:20 +0500

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  const [refresh, setRefresh] = useState(false);
+
+  useEffect(() => {
+    // then method
+    // axios.get("https://www.reddit.com/r/reactjs.json").then((res) => {
+    //   const newPosts = res.data.data.children.map((obj) => obj.data);
+    //   setPosts(newPosts);
+    // });
+
+    // async method
+    (async () => {
+      let response = await axios.get("https://www.reddit.com/r/reactjs.json");
+      let children = response.data.data.children;
+      let newPosts = children.map((obj) => {
+        return obj.data;
+      });
+      setPosts(newPosts);
+    })()
+  }, [refresh]);
+
+  console.log("this is our api data", posts);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
+
+      <button onClick={() => setRefresh(true)}>Refresh</button>
     </div>
   );
 }
